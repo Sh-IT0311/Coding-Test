8ace6bfc0da18a1983e4fa5ad41fb5b622ae67ba
Park SuHyun <golfpak0311@naver.com>
2024-03-21 00:18:36 +0900

    [Docs] Update 징검다리

--- softeer/징검다리.py
+++ softeer/징검다리.py
@@ -1,21 +1,15 @@
 import sys
 from bisect import bisect_left
+
 input = sys.stdin.readline
+N = input()
+A = tuple(map(int, input().split()))
 
-N = int(input())
-data = tuple(map(int, input().split()))
-
-lis = [data[0]]
-lidx = 0
-
-i = 1
-while i < N:
-    if lis[-1] < data[i]:
-        lis.append(data[i])
-        lidx += 1
+temp = [A[0]]
+for i in range(1, len(A)):
+    if temp[-1] < A[i]:
+        temp.append(A[i])
     else:
-        lis[bisect_left(lis, data[i])] = data[i]
+        temp[bisect_left(temp, A[i])] = A[i]
 
-    i += 1
-
-print(lidx + 1)
+print(len(temp))
